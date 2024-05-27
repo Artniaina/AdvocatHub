73505f04f36bd4941aa02bce51f4bd9037362cb8
Kantooo <kanto.andriahariniaina@gmail.com>
2024-05-27 17:47:45 +0300

    Modif de la page d'acceuil

--- src/Components/Homepage/Acceuil/ContenuAcceuil.jsx
+++ src/Components/Homepage/Acceuil/ContenuAcceuil.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import "../../Styles/Homepage/Acceuil/Acceuil.css";
 
 const ContenuAcceuil = () => {
   return (
-    <div>ContenuAcceuil</div>
-  )
-}
+    <div className="mainContainer">
+      <div className="container">
+        <h1>
+          Informations personnelles
+          <hr />
+        </h1>
+        <div className="containerInfo">
+          <p>
+            Nom: <br /> John Doe
+          </p>
+          <p>Prénom: <br /> Mathieu</p>
+          <p>Genre M/F: <br /> M</p>
+          <p>Nationalité <br /> France</p>
+          <p>Date de naissance: <br />121212</p>
+          <p>Lieu de naissance: <br />121212</p>
+          <p>Adresse privée: <br />121212</p>
+          <p>Telephone mobile: <br />121212</p>
+          <p>E-mail privé: <br />121212</p>
+          <p>IBAN: <br />121212</p>
+          <p>Code BIC: <br />121212</p>
+        </div>
+      </div>
+      <div className="container">
+        <h1>
+          Informations professionnelles <hr />
+        </h1>
 
-export default ContenuAcceuil
+        <div className="containerInfo">
+          <p>
+            Nom: <br /> John Doe
+          </p>
+          <p>Âge: 30 ans</p>
+          <p>Adresse: 1234 Rue Exemple, Ville, Pays</p>
+          <p>Email: johndoe@example.com</p>
+        </div>
+      </div>
+      <div className="container">
+        <h1>
+          Etude
+          <hr />
+        </h1>
+        <div className="containerInfo">
+          <p>
+            Nom: <br /> John Doe
+          </p>
+          <p>Âge: 30 ans</p>
+          <p>Adresse: 1234 Rue Exemple, Ville, Pays</p>
+          <p>Email: johndoe@example.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContenuAcceuil;
---
+++ src/Components/Homepage/Acceuil/Welcome.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+const Welcome = () => {
+  return (
+    <div className="mainContainer">
+      <p>Bienvenue</p>
+    </div>
+  );
+};
+
+export default Welcome;
