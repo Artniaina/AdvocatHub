48f45c0dc3129d43950b4810dd782b7399f84259
Kantooo <kanto.andriahariniaina@gmail.com>
2024-05-28 16:07:55 +0300

    Modif UI et ajout de donnée statique

--- src/Components/Homepage/Acceuil/ContenuAcceuil.jsx
+++ src/Components/Homepage/Acceuil/ContenuAcceuil.jsx
@@ -1,56 +1,152 @@
 import React from "react";
 import "../../Styles/Homepage/Acceuil/Acceuil.css";
+import Etude from "../../../assets/icons8-voir-le-fichier-100.png"
+import Pro from "../../../assets/icons8-lawyer-64.png"
+import Perso from "../../../assets/icons8-contact-d'affaires-100.png"
 
 const ContenuAcceuil = () => {
   return (
     <div className="mainContainer">
       <div className="container">
+
+      <img src={Perso} alt="logo" className="logo"/>
         <h1>
           Informations personnelles
           <hr />
         </h1>
         <div className="containerInfo">
           <p>
-            Nom: <br /> John Doe
+            Nom: <br /> John 
           </p>
-          <p>Prénom: <br /> Mathieu</p>
-          <p>Genre M/F: <br /> M</p>
-          <p>Nationalité <br /> France</p>
-          <p>Date de naissance: <br />121212</p>
-          <p>Lieu de naissance: <br />121212</p>
-          <p>Adresse privée: <br />121212</p>
-          <p>Telephone mobile: <br />121212</p>
-          <p>E-mail privé: <br />121212</p>
-          <p>IBAN: <br />121212</p>
-          <p>Code BIC: <br />121212</p>
+          <p>
+            Prénom: <br /> Mathieu
+          </p>
+          <p>
+            Genre M/F: <br /> M
+          </p>
+          <p>
+            Nationalité <br /> France
+          </p>
+          <p>
+            Date de naissance: <br />
+            121212
+          </p>
+          <p>
+            Lieu de naissance: <br />
+            121212
+          </p>
+          <p>
+            Adresse privée: <br />
+            121212
+          </p>
+          <p>
+            Telephone mobile: <br />
+            121212
+          </p>
+          <p>
+            E-mail privé: <br />
+            121212
+          </p>
+          <p>
+            IBAN: <br />
+            121212
+          </p>
+          <p>
+            Code BIC: <br />
+            121212
+          </p>
         </div>
       </div>
       <div className="container">
+      <img src={Pro} alt="logo" className="logo" style={{margin:"0px auto", width:"110px"}}/>
         <h1>
           Informations professionnelles <hr />
         </h1>
 
         <div className="containerInfo">
           <p>
-            Nom: <br /> John Doe
+            Identifiant Interne: <br /> 3 
           </p>
-          <p>Âge: 30 ans</p>
-          <p>Adresse: 1234 Rue Exemple, Ville, Pays</p>
-          <p>Email: johndoe@example.com</p>
+          <p>
+            Liste: <br /> Avocat à la Cour
+          </p>
+          <p>
+           E-mail barreau <br /> M
+          </p>
+          <p>
+            E-mail professionnel <br /> France
+          </p>
+          <p>
+           Date d'assermentation <br />
+            121212
+          </p>
+          <p>
+           Date d'avoué <br />
+            121212
+          </p>
+          <p>
+            Langue parlées: <br />
+            121212
+          </p>
+          <p>
+            Activités préférentielles: <br />
+            121212
+          </p>
+          <p>
+            Assistance Judiciaire <br />
+            <button>Oui</button>
+            <button>Non</button>
+          </p>
         </div>
       </div>
       <div className="container">
+      <img src={Etude} alt="logo" className="logo" />
+
         <h1>
           Etude
           <hr />
         </h1>
         <div className="containerInfo">
           <p>
-            Nom: <br /> John Doe
+            Dénomination <br /> LUTHER S.A 
           </p>
-          <p>Âge: 30 ans</p>
-          <p>Adresse: 1234 Rue Exemple, Ville, Pays</p>
-          <p>Email: johndoe@example.com</p>
+          <p>
+            Numéro voie <br /> 1B
+          </p>
+          <p>
+            Adresse <br /> Hei
+          </p>
+          <p>
+            Nationalité <br /> France
+          </p>
+          <p>
+            Date de naissance: <br />
+            121212
+          </p>
+          <p>
+            Lieu de naissance: <br />
+            121212
+          </p>
+          <p>
+            Adresse privée: <br />
+            121212
+          </p>
+          <p>
+            Telephone mobile: <br />
+            121212
+          </p>
+          <p>
+            E-mail privé: <br />
+            121212
+          </p>
+          <p>
+            IBAN: <br />
+            121212
+          </p>
+          <p>
+            Code BIC: <br />
+            121212
+          </p>
         </div>
       </div>
     </div>
--- src/Components/Homepage/Navbar.jsx
+++ src/Components/Homepage/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
 import { IoHome } from "react-icons/io5";
 import { IoDocumentTextSharp } from "react-icons/io5";
@@ -9,6 +9,8 @@ import logo from "../../assets/logo.png";
 import "../Styles/Homepage/Navbar.css";
 
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
@@ -16,32 +18,44 @@ const Navbar = () => {
       </div>
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link to="/home"  className="nav-link">
+          <Link 
+            to="/home"  
+            className={`nav-link ${location.pathname === "/home" || location.pathname === "/home" ? "active" : ""}`}
+          >
             <IoHome className="icon-nav" />
             Acceuil
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/document"  className="nav-link">
+          <Link 
+            to="/document"  
+            className={`nav-link ${location.pathname === "/document" ? "active" : ""}`}
+          >
             <IoDocumentTextSharp className="icon-nav" />
             Document
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/lbc" className="nav-link">
+          <Link 
+            to="/lbc" 
+            className={`nav-link ${location.pathname === "/lbc" ? "active" : ""}`}
+          >
             <FaBookBookmark className="icon-nav" />
             LBC/FT
           </Link>
         </li>
         <li className="nav-item">
-          <Link to="/faq" className="nav-link">
+          <Link 
+            to="/faq" 
+            className={`nav-link ${location.pathname === "/faq" ? "active" : ""}`}
+          >
             <TbDeviceIpadQuestion className="icon-nav" />
             FAQ
           </Link>
         </li>
       </ul>
       <div className="nav-user">
-        <Link className="nav-user">
+        <Link to="/profile" className="nav-user">
           <FaRegUserCircle />
         </Link>
         <button className="btn-nav">
