735a08621f52d87d9fff04044667550eb23802b7
Kantooo <kanto.andriahariniaina@gmail.com>
2024-08-09 17:20:33 +0300

    Add Style: success pop-up

--- src/Components/PopUp/PopUpSuccess.jsx
+++ src/Components/PopUp/PopUpSuccess.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import "../../Styles/PopUp/SuccessPopup.css";
-
+import { MdOutlineAttachEmail } from "react-icons/md";
 const SuccessPopup = ({  onGenerateAndSendPDF }) => {
   return (
     <div className="success-popup-overlay">
       <div className="success-popup-content">
-        <p>Success!</p>
+        <div className="violet-top"></div>
+        <div>
+        <MdOutlineAttachEmail style={{fontSize:"35px", "color":"#6e1992"}}/>
+        <p>Le certificat a été envoyé avec succès.</p>
         <button className="success-popup-button" onClick={onGenerateAndSendPDF}>OK</button>
+        </div>
       </div>
     </div>
   );
