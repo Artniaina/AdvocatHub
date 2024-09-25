ab47bce24a13e0015f4420e8f8e56ce4bbea1333
Kantooo <kanto.andriahariniaina@gmail.com>
2024-09-25 08:46:46 +0300

    Update: Add API integration for user-based form deletion and retrieval

--- src/Pages/ListeFormulairePage.jsx
+++ src/Pages/ListeFormulairePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../Components/Navbar";
 import { IoAddCircleSharp } from "react-icons/io5";
 import { RiDeleteBin5Line } from "react-icons/ri";
@@ -6,9 +6,59 @@ import { GrUpdate } from "react-icons/gr";
 import { PiNotePencil } from "react-icons/pi";
 import { useNavigate } from "react-router-dom";
 import "../Styles/TaxationForm/ListeForm.css"; 
+import { useAuth } from "../Hooks/AuthContext"; 
 
 const ListeFormulairePage = () => {
   const navigate = useNavigate();
+  const { user } = useAuth();
+  const [formulaires, setFormulaires] = useState([]);
+
+  const fetchFormulaires = async () => {
+    try {
+      const response = await fetch(`http://192.168.10.10/Utilisateur/GetListFormulaire/${user.email}`, {
+        method: 'GET', 
+      });
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      if (!data || data.length === 0) {
+        console.log("Il n'y a pas de formulaires disponibles."); 
+        setFormulaires([]); 
+      } else {
+        setFormulaires(data); 
+      }
+    } catch (error) {
+      console.error("Error fetching formulaires:", error);
+    }
+  };
+
+  const deleteFormulaire = async (idFormulaire) => {
+    setFormulaires((prevFormulaires) =>
+        prevFormulaires.filter((form) => form.sIDFormulaire !== idFormulaire)
+      );
+
+    try {
+      const response = await fetch(`http://192.168.10.10/Utilisateur/DeleteForm/${idFormulaire}`, {
+        method: 'DELETE', 
+      });
+      if (!response.ok) {
+        throw new Error('Failed to delete the form');
+      }
+      setFormulaires((prevFormulaires) =>
+        prevFormulaires.filter((form) => form.id !== idFormulaire)
+      );
+    } catch (error) {
+      console.error('Error deleting formulaire:', error);
+    }
+
+    fetchFormulaires();
+  };
+
+  useEffect(() => {
+    fetchFormulaires();
+  }, []);
+
 
   const handleNavigateAddNew = () => {
     navigate("/home/formTaxation");
@@ -18,9 +68,7 @@ const ListeFormulairePage = () => {
     navigate("/home/UpdateformTaxation");
   };
 
-  const handleDelete = () => {
-    navigate("/home/UpdateformTaxation");
-  };
+
 
   return (
     <>
@@ -49,28 +97,19 @@ const ListeFormulairePage = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Affaire 1</td>
-            <td>2024-09-01</td>
-            <td>En cours</td>
-            <td>2024-09-02</td>
-            <td>12345</td>
-            <td className="actions">
-              <PiNotePencil onClick={handleNavigate} />
-              <RiDeleteBin5Line onClick={handleDelete} />
-            </td>
-          </tr>
-          <tr>
-            <td>Affaire 2</td>
-            <td>2024-08-25</td>
-            <td>Terminé</td>
-            <td>2024-08-26</td>
-            <td>67890</td>
-            <td className="actions">
-              <PiNotePencil onClick={handleNavigate} />
-              <RiDeleteBin5Line onClick={handleDelete} />
-            </td>
-          </tr>
+          {formulaires.map((formulaire) => (
+            <tr key={formulaire.sIDFormulaire}>
+              <td>{formulaire.sNomAffaire}</td>
+              <td>{formulaire.sDateContestation}</td>
+              <td>{formulaire.sEtatAvancement}</td>
+              <td>{formulaire.sSubmited_at}</td>
+              <td>{formulaire.sIDFormulaire}</td>
+              <td className="actions">
+                <PiNotePencil onClick={handleNavigate} />
+                <RiDeleteBin5Line onClick={() => deleteFormulaire(formulaire.sIDFormulaire)} />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
