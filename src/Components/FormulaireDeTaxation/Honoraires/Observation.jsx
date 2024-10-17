2fa11641c8055c9a5fe5eb362a7dd20d1f2a93e1
Kantooo <kanto.andriahariniaina@gmail.com>
2024-10-17 16:55:06 +0300

    Feature: Add length validation in text editors and format content as non-HTML

--- src/Components/FormulaireDeTaxation/Honoraires/Observation.jsx
+++ src/Components/FormulaireDeTaxation/Honoraires/Observation.jsx
@@ -1,25 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Editor from "../TextEditor/EditeurHTML";
 import { useGeneraliteContext } from "../../../Hooks/GeneraliteContext";
+import PopupHTMLEditorWarning from "../TextEditor/PopupHTMLEditorWarning"; 
 
 const Observation = () => {
   const { editorContents, setEditorContents } = useGeneraliteContext();
+  const [showWarning, setShowWarning] = useState(false);
+  const observationRef = useRef(null);
+  const [content, setContent] = useState(""); 
 
-  const handleEditorChange = (id, content) => {
+  const handleEditorChange = (id, newContent) => {
+    setContent(newContent); 
     setEditorContents((prevContent) => ({
       ...prevContent,
-      [id]: content,
+      [id]: newContent,
     }));
   };
 
+  const handleClosePopup = () => {
+    setShowWarning(false);
+  };
+
+  const handleClickOutside = (event) => {
+    if (observationRef.current && !observationRef.current.contains(event.target)) {
+      const plainText = extractPlainText(content);
+      console.log("Plain Text:", plainText, plainText.length);
+
+      if (plainText.length < 13 && plainText.trim() !== "") {
+        setShowWarning(true);
+      }
+    }
+  };
+
+  const extractPlainText = (html) => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    return doc.body.textContent || "";
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [content]);
+
   return (
-    <>
+    <div ref={observationRef}> 
       <Editor
         key="observation"
         id="observation"
-        onChange={(content) => handleEditorChange("observation", content)}
+        onChange={(newContent) => handleEditorChange("observation", newContent)}
       />
-    </>
+      {showWarning && (
+        <PopupHTMLEditorWarning 
+          onClose={handleClosePopup} 
+          nomChamp="observation" 
+        />
+      )}
+    </div>
   );
 };
 
