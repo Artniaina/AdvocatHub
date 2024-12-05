026c01b9f7bed32c77c20f0056f34815051528ba
Kantooo <kanto.andriahariniaina@gmail.com>
2024-12-05 14:43:49 +0300

    Fix: Correct Word file rendering display

--- src/Components/Document/DocumentList/DocumentList.jsx
+++ src/Components/Document/DocumentList/DocumentList.jsx
@@ -1,9 +1,28 @@
 import React, { useState } from "react";
 import SearchBar from "../SearchBar";
 import "../../../Styles/Document/DocumentTable.css";
-import DocViewer, { DocViewerRenderers } from "react-doc-viewer";
+import  { DocViewerRenderers } from "react-doc-viewer";
+import DocViewer from "./doc.js";
 
 const DocumentList = () => {
+  const DocIframe = ({ source }) => {
+    if (!source) {
+      return <div>Loading...</div>;
+    }
+  
+    const src = source;
+    return (
+      <div>
+        <iframe
+          src={"https://docs.google.com/viewer?url=" + src + "&embedded=true"}
+          title="file"
+          width="100%"
+          height="600"
+        ></iframe>
+      </div>
+    );
+  };
+  
   const documents = [
     {
       name: "Document 1",
@@ -18,7 +37,7 @@ const DocumentList = () => {
       description: "This is a description for Document 2.",
       lastModified: "2024-12-02",
       version: "2.0",
-      wordUrl: require("./Compte rendu de la journée.docx"),  // Use `wordUrl` or similar to represent the word file.
+      wordUrl: "https://www.academia.edu/35362398/Amour_docx",
     },
     {
       name: "Document 3",
@@ -28,25 +47,11 @@ const DocumentList = () => {
       pdfUrl: "",
     },
   ];
-  const docs = [
-    // {
-    //   uri:
-    //     "http://localhost:9000/uploads/ULRYB3ATJ56B/Screenshot%202021-04-28%20at%2014.04.23.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=minio%2F20210507%2F%2Fs3%2Faws4_request&X-Amz-Date=20210507T142426Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&X-Amz-Signature=761187860be22801088ab8c212733f7f52af8f62d638f1341ee2ae4c18944251"
-    //   // "http://localhost:9000/uploads/6QK5HJ84MAEM/RAS-118_CompanyCodes__SalesOffices.xlsx?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=minio%2F20210507%2F%2Fs3%2Faws4_request&X-Amz-Date=20210507T110429Z&X-Amz-Expires=432000&X-Amz-SignedHeaders=host&X-Amz-Signature=c20f9b77ffdc1a15910cea5acd3420b6583a1d4d38ce5716da30f1d0ea4315d5"
-    //   // "https://res.cloudinary.com/cloudinaryforme/image/upload/v1618339571/workplace-1245776_1920_i9ayae.jpg"
-    // },
 
-    // {
-    //   uri:
-    //     "https://code.visualstudio.com/shortcuts/keyboard-shortcuts-macos.pdf"
-    // },
-    // { uri: require("./test-excelaki.xlsx") },
-    // { uri: require("./Compte rendu de la journée.docx") }
-  ];
   const [filteredDocuments, setFilteredDocuments] = useState(documents);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [currentDocumentUrl, setCurrentDocumentUrl] = useState(null);
-  const [documentType, setDocumentType] = useState(""); // Track document type (PDF/Word)
+  const [documentType, setDocumentType] = useState("");
 
   const openModal = (url, type) => {
     setCurrentDocumentUrl(url);
@@ -59,7 +64,6 @@ const DocumentList = () => {
     setCurrentDocumentUrl(null);
     setDocumentType("");
   };
-
   return (
     <div>
       <SearchBar
@@ -130,8 +134,10 @@ const DocumentList = () => {
                 </p>
               </object>
             ) : documentType === "word" ? (
-              <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />
-
+              <div className="App">
+              <h1>Sample Doc file:</h1>
+              <DocViewer source="https://www.academia.edu/35362398/Amour_docx" />
+            </div>
             ) : (
               <p>No document available</p>
             )}
